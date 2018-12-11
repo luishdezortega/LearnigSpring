992d32393d10104948babeba08137e00b0d63253
Luis Carlos Hernández Ortega <luis.hernandez@PRAGMA.COM.CO>
2018-12-11 10:02:12 -0500

    Implementando el DAO

--- ProfesoresCatalogo/src/main/java/com/pragma/profesores/dao/AbstractSession.java
+++ ProfesoresCatalogo/src/main/java/com/pragma/profesores/dao/AbstractSession.java
@@ -1,8 +1,18 @@
 package com.pragma.profesores.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public abstract class AbstractSession {
 	
 	
+	@Autowired
+	private SessionFactory sessionFactory;
 	
+	
+		protected Session getSession() {
+			return sessionFactory.getCurrentSession();
+		}
 
 }
--- ProfesoresCatalogo/src/main/java/com/pragma/profesores/dao/ProfesorDaoImPlements.java
+++ ProfesoresCatalogo/src/main/java/com/pragma/profesores/dao/ProfesorDaoImPlements.java
@@ -7,43 +7,41 @@ import com.pragma.profesores.model.Profesor;
 
 
 
-public class ProfesorDaoImPlements implements ProfesorDao {
+public class ProfesorDaoImPlements extends AbstractSession implements ProfesorDao {
 	
 	
 	
 
 	public void guardarProfesor(Profesor profesor) {
-	
-		
+			getSession().persist(profesor);
 	}
 
 	public void borrarProfesor(Long id_Profesor) {
-		// TODO Auto-generated method stub
 		
+		Profesor profe = buscarProfeID(id_Profesor);
+		if(profe != null) {
+			getSession().delete(profe);
+		}
 	}
 
 	public void actualizarProfesor(Profesor profesor) {
-		// TODO Auto-generated method stub
 		
+		getSession().update(profesor);
 	}
 
 	public List<Profesor> listarProfes() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return getSession().createQuery("from Profesor").list();
 	}
 
 	public Profesor buscarProfeID(Long id_Profesor) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return (Profesor) getSession().get(Profesor.class,id_Profesor);
 	}
 
-	public Profesor buscarProfeNombre(String name) {
-		// TODO Auto-generated method stub
-		return null;
+	public Profesor buscarProfeNombre(String nombre) {
+		
+		return (Profesor) getSession().createQuery("from Profesor where nombre_P = :nombre_P").setParameter("nombre_P", nombre).uniqueResult();
 	}
 
-	
-	
-	
-	
 }
